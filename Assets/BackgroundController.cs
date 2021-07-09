4b626e9ca6755cb6418345bcb89e7e3c39c9862e
tmizobuchi3 <83824309+tmizobuchi3@users.noreply.github.com>
2021-07-10 00:50:58 +0900

    Initial commit

---
+++ Assets/BackgroundController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    //スクロール速度
+    private float scrollSpeed = -1;
+
+    //背景終了位置
+    private float deadLine = -16;
+
+    private float startLine = 15.8f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //背景を移動する
+        transform.Translate(this.scrollSpeed * Time.deltaTime, 0, 0);
+
+        //画面買いに出たら、画面右端に移動する
+        if(transform.position.x < this.deadLine)
+        {
+            transform.position = new Vector2(startLine, 0);
+        }
+    }
+}
---
+++ Assets/CubeController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    //キューブの移動速度
+    private float speed = -12;
+
+    //消滅位置
+    private float deadLine = -10;
+
+    //地面の位置★
+    private float groundLebel1 = -3.0f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //★
+        if (transform.position.y = -0.3f)
+        {
+            //着地しているかどうかを調べる★
+            bool isGround = (transform.position.y = this.groundLebel1) ? false : true;
+
+            //着地時に音を出す★
+            GetComponent<AudioSource>().volume = (isGround) ? 0 : 1;
+        }
+
+
+        //キューブを移動させる
+        transform.Translate(this.speed * Time.deltaTime, 0, 0);
+
+        //画面買いに出たら破棄する
+        if(transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
