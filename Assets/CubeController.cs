3b94e3478f97558cc93f69c81050dd9d2bd19022
tmizobuchi3 <83824309+tmizobuchi3@users.noreply.github.com>
2021-07-16 21:35:06 +0900

    修正
    スクリプトの修正を行いました。

--- Assets/CubeController.cs
+++ Assets/CubeController.cs
@@ -4,37 +4,23 @@ using UnityEngine;
 
 public class CubeController : MonoBehaviour
 {
+    GameObject Cube;
+
     //キューブの移動速度
     private float speed = -12;
 
     //消滅位置
     private float deadLine = -10;
 
-    //地面の位置★
-    private float groundLebel1 = -3.0f;
-
-
-
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cube = GameObject.Find("CubePrefab");
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        //★
-        if (transform.position.y = -0.3f)
-        {
-            //着地しているかどうかを調べる★
-            bool isGround = (transform.position.y = this.groundLebel1) ? false : true;
-
-            //着地時に音を出す★
-            GetComponent<AudioSource>().volume = (isGround) ? 0 : 1;
-        }
-
 
         //キューブを移動させる
         transform.Translate(this.speed * Time.deltaTime, 0, 0);
@@ -42,7 +28,20 @@ public class CubeController : MonoBehaviour
         //画面買いに出たら破棄する
         if(transform.position.x < this.deadLine)
         {
+            //Debug.Log("破棄");
             Destroy(gameObject);
         }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag != "UnityChan2DTag")
+        {
+            //Debug.Log("再生");
+            AudioSource audio = GetComponent<AudioSource>();
+            audio.Play();
+        }
+
+
     }
 }
